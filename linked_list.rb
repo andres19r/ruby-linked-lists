6d5080b0f2b7940324044c03ff964852100af7cb
Andres Rivero <andres19rivero@gmail.com>
2021-08-31 23:17:48 -0400

    feat: #size, #head, #tail, #at

--- linked_list.rb
+++ linked_list.rb
@@ -10,12 +10,45 @@ class LinkedList
   def append(value)
     n = new Node
     n.value = value
-    @head = n if @head.nil?
+    if @head.nil?
+      prepend(value)
+    else
+      tmp = @head
+      tmp = tmp.next_node until tmp.next_node.nil?
+      tmp.next_node = n
+    end
   end
 
   def prepend(value)
     n = new Node
     n.value = value
     @head = n
+  end
+
+  def size
+    s = 0
+    tmp = @head
+    s += 1 until tmp.next_node.nil?
+    s
+  end
+
+  def head
+    @head
+  end
+
+  def tail
+    tmp = @head
+    tmp = tmp.next_node until tmp.next_node.nil?
+    tmp
+  end
+
+  def at(index)
+    i = 0
+    tmp = @head
+    until tmp.next_node.nil?
+      tmp if index == i
+      tmp = tmp.next_node
+    end
+    'index not found'
   end
 end
